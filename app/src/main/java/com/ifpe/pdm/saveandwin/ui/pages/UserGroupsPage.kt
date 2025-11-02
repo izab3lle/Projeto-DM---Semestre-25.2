e75d24c5d47e618a530937258c315790f1114f8e
Izabelle <izabelle_tais@outlook.com>
2025-11-02 13:59:50 -0300

    Páginas de teste para a navegação

---
+++ app/src/main/java/com/ifpe/pdm/saveandwin/ui/pages/UserGroupsPage.kt
@@ -0,0 +1,34 @@
+package com.ifpe.pdm.saveandwin.ui.pages
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.ifpe.pdm.saveandwin.viewmodel.MainViewModel
+
+@Composable
+fun UserGroupsPage(modifier: Modifier = Modifier.Companion, viewModel: MainViewModel) {
+    Column(
+        modifier = modifier.fillMaxSize()
+            .background(Color.Blue)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Seus Grupos",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = modifier.align(CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
