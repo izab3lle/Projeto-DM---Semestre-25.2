61b1899e94acb1e24df5a40ca5a78bf8f59084bf
Izabelle <izabelle_tais@outlook.com>
2025-11-18 20:16:22 -0300

    Card de Grupo

--- app/src/main/java/com/ifpe/pdm/saveandwin/ui/pages/UserGroupsPage.kt
+++ app/src/main/java/com/ifpe/pdm/saveandwin/ui/pages/UserGroupsPage.kt
@@ -1,34 +1,129 @@
 package com.ifpe.pdm.saveandwin.ui.pages
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardDefaults.elevatedCardColors
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ifpe.pdm.saveandwin.model.Group
+import com.ifpe.pdm.saveandwin.ui.theme.GrayDescriptionColor
+import com.ifpe.pdm.saveandwin.ui.theme.GreenSW
+import com.ifpe.pdm.saveandwin.ui.theme.LightGrayField
+import com.ifpe.pdm.saveandwin.ui.theme.LightGreenBackground
+import com.ifpe.pdm.saveandwin.viewmodel.GroupPageViewModel
 import com.ifpe.pdm.saveandwin.viewmodel.MainViewModel
+import kotlin.collections.remove
 
 @Composable
-fun UserGroupsPage(modifier: Modifier = Modifier.Companion, viewModel: MainViewModel) {
+fun UserGroupsPage(modifier: Modifier = Modifier.Companion, viewModel: GroupPageViewModel) {
+    val groups = viewModel.groups
+
     Column(
         modifier = modifier.fillMaxSize()
-            .background(Color.Blue)
-            .wrapContentSize(Alignment.Center)
+            .background(LightGreenBackground)
+            .padding(horizontal = 20.dp)
+            .padding(top = 10.dp),
+        horizontalAlignment = CenterHorizontally,
     ) {
-        Text(
-            text = "Seus Grupos",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = modifier.align(CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize()
+        ) {
+            item {
+                Text(
+                    text = "Seus Grupos",
+                    modifier = Modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Start,
+                    fontSize = 30.sp
+                )
+                Spacer(Modifier.size(20.dp))
+            }
+
+            items(groups, key = { it.name }) { group ->
+                GroupCard(group)
+                Spacer(Modifier.size(15.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun GroupCard(group: Group) {
+    val titleSize = 18.sp
+    val contentSize = 14.sp
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier.fillMaxWidth(),
+        colors = elevatedCardColors(
+            containerColor = Color.White
         )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(110.dp),
+        ) {
+            Image(
+                painter = painterResource(group.image),
+                contentDescription = "Imagem do grupo",
+                contentScale = ContentScale.Crop
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)
+        ) {
+           Text(
+               text = group.name,
+               fontWeight = FontWeight.Bold,
+               fontSize = titleSize
+           )
+            Text(
+                text = "${group.members.size} integrantes",
+                fontWeight = FontWeight.Medium,
+                fontSize = contentSize
+            )
+            group.description?.let {
+                Text(
+                    text = it,
+                    fontSize = contentSize,
+                    color = GrayDescriptionColor
+                )
+            }
+        }
     }
 }
