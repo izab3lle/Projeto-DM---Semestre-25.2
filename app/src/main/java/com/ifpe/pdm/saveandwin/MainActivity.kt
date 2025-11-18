4541b83a917b9ba9f4b6c7f2c653c02452ce6e5e
Izabelle <izabelle_tais@outlook.com>
2025-11-18 20:15:16 -0300

    Icons e cores na TopAppBar

--- app/src/main/java/com/ifpe/pdm/saveandwin/MainActivity.kt
+++ app/src/main/java/com/ifpe/pdm/saveandwin/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -18,18 +24,24 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.ifpe.pdm.saveandwin.ui.nav.BottomNavBar
 import com.ifpe.pdm.saveandwin.ui.nav.BottomNavItem
 import com.ifpe.pdm.saveandwin.ui.nav.MainNavHost
+import com.ifpe.pdm.saveandwin.ui.theme.GreenSW
 import com.ifpe.pdm.saveandwin.ui.theme.SaveAndWinTheme
+import com.ifpe.pdm.saveandwin.viewmodel.GroupPageViewModel
 import com.ifpe.pdm.saveandwin.viewmodel.MainViewModel
 
 class MainActivity : ComponentActivity() {
@@ -39,22 +51,36 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             val viewModel : MainViewModel by viewModels()   // State Hoisting - Deve ser declaraod no topo, para não ser reinicializado
+            val groupPageViewModel : GroupPageViewModel by viewModels()
 
             SaveAndWinTheme {
 
                 Scaffold(
                     topBar = {
                         @OptIn(ExperimentalMaterial3Api::class)
-                        TopAppBar(
-                            title = { Text("Bem-vindo/a!") },
-                            actions = {
-                                IconButton( onClick = { finish() } ) {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                Image(
+                                    painter = painterResource(R.drawable.money_logo),
+                                    contentDescription = "Logo Save & Win",
+                                    modifier = Modifier.size(40.dp)
+                                )
+                                    },
+                            navigationIcon = {
+                                IconButton(onClick = { this.finish() }) {
                                     Icon(
-                                        imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                                        imageVector = Icons.Default.Menu,
                                         contentDescription = "Localized description"
                                     )
                                 }
-                            }
+                            },
+                            colors = TopAppBarColors(
+                                containerColor = Color.White,
+                                scrolledContainerColor = Color.White,
+                                navigationIconContentColor = Color.Black,
+                                titleContentColor = Color.Black,
+                                actionIconContentColor = Color.Black
+                            )
                         )
                     },
                     bottomBar = {
@@ -65,13 +91,21 @@ class MainActivity : ComponentActivity() {
                         BottomNavBar(navController = navController, items)
                     },
                     floatingActionButton = {
-                        FloatingActionButton(onClick = { }) {
+                        FloatingActionButton(
+                            onClick = { },
+                            containerColor = GreenSW,
+                            shape = CircleShape
+                        ) {
                             Icon(Icons.Default.Add, contentDescription = "Adicionar")
                         }
                     }
                 ) { innerPadding ->
                     Box(modifier = Modifier.padding(innerPadding)) {
-                        MainNavHost(navController = navController, viewModel = viewModel)
+                        MainNavHost(
+                            navController = navController,
+                            viewModel = viewModel,
+                            groupPageViewModel = groupPageViewModel
+                        )
                     }
                 }
             }
